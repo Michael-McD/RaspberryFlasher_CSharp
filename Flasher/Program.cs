38b6d3e139b1e05cfec146d383146fbf9000f582
Michael McDowell <michael@pop-os.localdomain>
2020-05-20 17:28:59 +0100

    remove obj folder

---
+++ Flasher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Flasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
