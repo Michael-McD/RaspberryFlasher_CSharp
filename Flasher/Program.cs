82d0179fff77beca80cba46360d38c0262b57583
Michael McDowell <michael@pop-os.localdomain>
2020-05-23 22:21:55 +0100

    Build task working as expected.

--- Flasher/Program.cs
+++ Flasher/Program.cs
@@ -6,7 +6,7 @@ namespace Flasher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Mr. Pie!");
         }
     }
 }
